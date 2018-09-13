41d68899b1b65c5b2a36c7aeed8f66a87bd58074
yangyzp <513993979@qq.com>
2018-09-13 12:42:09 +0800

    Update docker_shell.sh

--- docker_shell.sh
+++ docker_shell.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #Check Root
-cd /home
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager \
+    --add-repo \
+     https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum makecache fast
+sudo yum install docker-ce  -y

b67a4227cdb3ba84fe43d4578a7ef019ad41dd61
yangyzp <513993979@qq.com>
2019-12-01 21:53:06 +0800

    Update docker_clear.sh

--- docker_clear.sh
+++ docker_clear.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #Check Root
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager \
-    --add-repo \
-     https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum makecache fast
-sudo yum install docker-ce  -y
-sudo systemctl start docker
-sudo systemctl enable docker
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker image rm $(docker image ls -a -q)
+docker volume rm $(docker volume ls -q)

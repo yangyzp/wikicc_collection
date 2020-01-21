ae5dd1707ea1b82c0942604371f2a174ed5e5186
yangyzp <513993979@qq.com>
2020-01-21 19:50:57 +0800

    Add files via upload

---
+++ cronta_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Check Root
+echo "11 4 * * * sudo service docker restart" >> /var/spool/cron/root
+echo "18 4 */2 * * /sbin/reboot" >> /var/spool/cron/root
+service crond restart
+chkconfig crond on

1a592232368b5130ed7c1908ef82753f55d91957
yangyzp <513993979@qq.com>
2020-07-03 01:04:17 +0800

    Update cronta_docker.sh

--- cronta_docker.sh
+++ cronta_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Check Root
 echo "11 4 * * * sudo service docker restart" >> /var/spool/cron/root
-echo "18 4 */2 * * /sbin/reboot" >> /var/spool/cron/root
+echo "18 4 * * * /sbin/reboot" >> /var/spool/cron/root
 service crond restart
 chkconfig crond on
 systemctl stop firewalld.service

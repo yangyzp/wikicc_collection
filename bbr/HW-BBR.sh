db75823ee27297a8002edfac2535b97e20982717
nico <513993979@qq.com>
2023-08-05 13:32:55 +0800

    BBR优化脚本
    BBR优化脚本

---
+++ bbr/HW-BBR.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cat > /etc/sysctl.conf <<EOF
+fs.file-max = 1048576
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
+net.core.default_qdisc = cake
+#ipv4
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_congestion_control = bbr
+net.ipv4.tcp_slow_start_after_idle = 0
+#forward
+net.ipv4.ip_forward = 1
+net.ipv4.conf.all.forwarding = 1
+net.ipv4.conf.default.forwarding = 1
+net.ipv4.conf.all.route_localnet = 1
+EOF
+sysctl -p && sysctl --system
